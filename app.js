2beb6f4bf8693f809f2a01167d532a44512db1e5
color <95304946+HermanMazur@users.noreply.github.com>
2022-12-09 15:00:07 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ const cors = require("cors");
 
 const booksRouter = require("./routes/api/books")
 
-
 const app = express();
 
 app.use(cors());
 
-app.use("/routes/api/books.js", booksRouter);
+app.use("/api/books", booksRouter);
 // /routes/api/books.js - для любой функции с апі/букс
 
 app.listen(3000);
